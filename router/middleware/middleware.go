32ee29b2460a3de01ad727a45551857ed7da5fbc
Muhammad Nur Fadli <mnurfadd@gmail.com>
2024-11-17 09:13:44 +0700

    moving, updating return err on authentication

--- router/middleware/middleware.go
+++ router/middleware/middleware.go
@@ -1,13 +1,11 @@
-package router
+package middleware
 
 import (
 	_const "billingg-engine/const"
 	"billingg-engine/model"
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"strings"
 	"time"
@@ -15,8 +13,7 @@ import (
 
 var jwtKey = []byte("very_secret_key")
 
-// generateJWT generates a new JWT token for the given username
-func generateJWT(username, role string) (string, error) {
+func GenerateJWT(username, role string) (string, error) {
 	expirationTime := time.Now().Add(1 * time.Hour)
 	claims := &model.UserClaims{
 		Username: username,
@@ -30,7 +27,6 @@ func generateJWT(username, role string) (string, error) {
 	return token.SignedString(jwtKey)
 }
 
-// verifyJWT checks if the provided token is valid
 func verifyJWT(tokenString string) (*model.UserClaims, error) {
 	claims := &model.UserClaims{}
 
@@ -45,63 +41,39 @@ func verifyJWT(tokenString string) (*model.UserClaims, error) {
 	return claims, nil
 }
 
-type Middleware struct {
-	userDataSource []model.User
-}
-
-func NewMiddleware(data []model.User) Middleware {
-	return Middleware{
-		userDataSource: data,
-	}
-}
-
-func (m Middleware) AuthenticateUser(username, password string) (string, error) {
-	for _, user := range m.userDataSource {
-		if user.Username == username {
-			err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
-			if err != nil {
-				return "", fmt.Errorf("invalid credentials")
-			}
-			return generateJWT(username, user.Role)
-		}
-	}
-	return "", fmt.Errorf("invalid credentials")
-}
-
-func (m Middleware) Login(w http.ResponseWriter, r *http.Request) {
-	tokenString, err := m.AuthenticateUser(r.FormValue("username"), r.FormValue("password"))
-	if err != nil {
-		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
-}
-
 func AuthMiddlewareWithRole(next func(http.ResponseWriter, *http.Request), role string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
+		w.Header().Set("Content-Type", "application/json")
+
+		type Err struct {
+			Error string `json:"error"`
+		}
+
 		if authHeader == "" {
-			http.Error(w, "Missing auth token", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(Err{Error: "Missing Auth Token"})
 			return
 		}
 
 		// Expecting token to start like "Bearer <token>"
 		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
 		if tokenStr == authHeader {
-			http.Error(w, "Missing or invalid Bearer token", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(Err{Error: "Missing or invalid Bearer token"})
 			return
 		}
 
 		claims, err := verifyJWT(tokenStr)
 		if err != nil {
-			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(Err{Error: "Invalid or expired token"})
 			return
 		}
 
 		if claims.Role != role {
-			http.Error(w, "you did not have permission to view this page", http.StatusForbidden)
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(Err{Error: "you did not have permission to view this page"})
 			return
 		}
 
